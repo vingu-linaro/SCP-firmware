b29ac4d55edbb023111fd08c68a2ef84eb9a18d4
Vincent Guittot <vincent.guittot@linaro.org>
2023-11-29 09:44:06 +0100

    arch/none: add a generic secure partition
    Add a new architecture to build the SCMI server in a secure partition.
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>

---
+++ arch/none/sp/src/arch_mm.c
@@ -0,0 +1,68 @@
+/*
+ * Renesas SCP/MCP Software
+ * Copyright (c) 2020-2022, Renesas Electronics Corporation. All rights
+ * reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <fwk_arch.h>
+#include <fwk_macros.h>
+#include <fwk_log.h>
+
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#define LOCAL_HEAP 258*1024
+static char local_heap[LOCAL_HEAP];
+
+/*!
+ * \brief Architecture memory manager context.
+ */
+static struct arch_mm_ctx {
+    uintptr_t heap_break;
+    uintptr_t heap_end;
+} arch_mm_ctx = {
+    .heap_break = (uintptr_t)(&local_heap[0]),
+    .heap_end = (uintptr_t)(&local_heap[LOCAL_HEAP]),
+};
+
+void *_sbrk(intptr_t increment)
+{
+    if (increment == 0) {
+        return (void *)arch_mm_ctx.heap_break;
+    } else {
+        uintptr_t heap_old = FWK_ALIGN_NEXT(arch_mm_ctx.heap_break, 16);
+        uintptr_t heap_new = heap_old + increment;
+
+        if (heap_new > arch_mm_ctx.heap_end) {
+            return (void *)-1;
+        } else {
+            arch_mm_ctx.heap_break = heap_new;
+
+            return (void *)heap_old;
+        }
+    }
+}
+
+void *malloc(size_t size)
+{
+    void *mem = _sbrk((intptr_t)size);
+
+    if (mem == ((void *)-1))
+        mem = NULL;
+
+    return mem;
+}
+
+void *calloc(size_t nmemb, size_t size)
+{
+    void *mem = malloc(nmemb * size);
+
+    if (mem)
+        memset(mem, 0, nmemb * size);
+
+    return mem;
+}
