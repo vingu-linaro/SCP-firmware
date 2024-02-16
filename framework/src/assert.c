524bc4fe067e91609d711d56ec6a20855875b91d
Vincent Guittot <vincent.guittot@linaro.org>
2024-02-16 12:07:17 +0100

    Revert "fwk/assert: print location of assertion"
    This reverts commit 0fb270a523836fcde0294fc4c601bb1062504c14.

--- framework/src/assert.c
+++ framework/src/assert.c
@@ -1,21 +1,16 @@
 /*
  * Arm SCP/MCP Software
- * Copyright (c) 2020-2024, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2020-2021, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <fwk_attributes.h>
-#include <fwk_io.h>
-
 #include <assert.h>
+#include <fwk_attributes.h>
 
 #if defined(__ARMCC_VERSION)
 void __aeabi_assert(const char *expr, const char *file, int line)
 {
-    (void)fwk_io_printf(
-        fwk_io_stdout, "Assertion failed at %s:%d\n", file, line);
-
     while (1) {
         continue;
     }
@@ -27,13 +22,6 @@ FWK_WEAK void __assert_func(
     const char *function,
     const char *assertion)
 {
-    (void)fwk_io_printf(
-        fwk_io_stdout,
-        "Assertion failed in %s at %s:%d\n",
-        function,
-        file,
-        line);
-
     while (1) {
         continue;
     }
